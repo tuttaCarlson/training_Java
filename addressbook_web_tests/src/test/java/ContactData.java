f3ec8fe191b7968f2d3f7f2cdde05d212f20a50d
ofominawork <olga.fomina.works@gmail.com>
2020-03-09 19:08:46 +0300

    ContactCreationTests

---
+++ addressbook_web_tests/src/test/java/ContactData.java
@@ -0,0 +1,36 @@
+public class ContactData {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String phoneMobile;
+
+    public ContactData(String firstName, String lastName, String address,
+                       String email, String phoneMobile){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phoneMobile = phoneMobile;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneMobile() {
+        return phoneMobile;
+    }
+}
