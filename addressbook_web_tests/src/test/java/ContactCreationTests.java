e884391c754dd6c57aa12cb12ec8e896fc881632
ofominawork <olga.fomina.works@gmail.com>
2020-03-09 22:37:33 +0300

    Refactoring : TestBase c;lass with common methods

--- addressbook_web_tests/src/test/java/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/ContactCreationTests.java
@@ -1,61 +1,13 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ContactCreationTests {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        driver.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-    }
+public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation(){
         initContactCreation();
-        fillContactForm(new ContactData("Rabbit", "Nice", "SPB Russia", "rabbit.nice.works@msailk.nu", "+79220002323"));
+        fillContactForm(new ContactData("Rabbit", "Nice", "SPB Russia",
+                "rabbit.nice.works@msailk.nu", "+79220002323"));
         submitContactCreation();
-        //returnToGroupPage();
     }
 
-    private void submitContactCreation(){
-        driver.findElement(By.name("submit")).click();
-    }
-
-    private void fillContactForm(ContactData contactData){
-        driver.findElement(By.name("firstname")).click();
-        driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-        driver.findElement(By.name("lastname")).click();
-        driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-        driver.findElement(By.name("address")).click();
-        driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        driver.findElement(By.name("mobile")).click();
-        driver.findElement(By.name("mobile")).sendKeys(contactData.getPhoneMobile());
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
-
-    private void initContactCreation(){
-        driver.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-    }
 }
--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,29 +1,6 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class GroupCreationTests {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        driver.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation(){
@@ -33,36 +10,5 @@ public class GroupCreationTests {
         submitGroupCreation();
         returnToGroupPage();
     }
-
-    private void returnToGroupPage() {
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    private void submitGroupCreation() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    private void openGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
-
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-    }
-
 
 }
