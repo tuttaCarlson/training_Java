f01dbfef2d4a12b616ac76f0fe47ec099f5b1be8
ofominawork <olga.fomina.works@gmail.com>
2020-03-09 17:45:29 +0300

    GroupCreationTests refactoring

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -14,25 +14,49 @@ public class GroupCreationTests {
         driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
         driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("user")).sendKeys(username);
         driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.name("pass")).sendKeys(password);
         driver.findElement(By.xpath("//input[@type='submit']")).click();
     }
 
     @Test
     public void testGroupCreation(){
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("Test");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("Test_header");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("Test_footer");
-        driver.findElement(By.name("submit")).click();
+        openGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("Test", "Test_header", "Test_footer"));
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    private void returnToGroupPage() {
         driver.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initGroupCreation() {
+        driver.findElement(By.name("new")).click();
+    }
+
+    private void openGroupPage() {
+        driver.findElement(By.linkText("groups")).click();
     }
 
     @AfterMethod
---
+++ addressbook_web_tests/src/test/java/GroupData.java
@@ -0,0 +1,25 @@
+public class GroupData {
+    private String name;
+    private String header;
+    private String footer;
+
+    public GroupData(String name, String header, String footer) {
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+
+}
