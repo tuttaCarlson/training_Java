e201bc3b81109d8049b8f031aff5a43969bab5cb
ofominawork <olga.fomina.works@gmail.com>
2020-03-24 12:37:20 +0300

    Lecture 6.3 : Command line options analysis while running generator
    JCommander is used for cmd parsing.

--- addressbook_web_tests/src/test/java/generators/GroupDataGenerator.java
+++ addressbook_web_tests/src/test/java/generators/GroupDataGenerator.java
@@ -1,5 +1,7 @@
 package generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import model.GroupData;
 
 import java.io.File;
@@ -10,15 +12,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
+    @Parameter (names = "-c", description = "element count")
+    public int count;
+
+    @Parameter (names = "-f", description = "target file")
+    public String file;
+
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-        List<GroupData> groups = generateGroups(count);
-        save (groups, file);
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander.newBuilder().addObject(generator).build().parse(args);
+        generator.run();
 
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
+    }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups){
@@ -28,7 +40,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for(int i=0; i<count; i++){
             groups.add(new GroupData().withName(String.format("test %s", i))
