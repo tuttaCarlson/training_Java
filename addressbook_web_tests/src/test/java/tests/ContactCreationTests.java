4669ae05a9a0eaad2408fbb5798c1484e478fe60
ofominawork <olga.fomina.works@gmail.com>
2020-03-31 22:37:45 +0300

    Task13 : Parametrize tests for groups and contacts
    The task for groups has been done earlier. Here the task is done for contacts (data providers which read data from XML file are added)

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,16 +1,19 @@
 package tests;
 
+import com.thoughtworks.xstream.XStream;
 import model.ContactData;
 import model.Contacts;
 import model.GroupData;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,30 +21,34 @@ import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase {
 
+    @BeforeClass
+    public void performPreconditions(){
+        app.goTo().groupPage();
+        GroupData group = new GroupData().withName("test")
+                .withHeader("header").withFooter("footer");
+        app.group().create(group);
+    }
+
     @DataProvider
     public Iterator<Object []> validContacts() throws IOException {
-        List<Object []> list = new ArrayList<Object[]>();
         BufferedReader reader = new BufferedReader
-                (new FileReader(new File("src/test/resources/contacts.csv")));
+                (new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
         String line = reader.readLine();
         while(line!=null){
-        String[] split = line.split(";");
-        ContactData contact = new ContactData().withFirstName(split[0]).withLastName(split[1])
-                .withAddress(split[2]).withEmail(split[3]).withPhoneMobile(split[4])
-                .withPhoto(new File(split[5])).withGroup(split[6]);
-        list.add(new Object[]{contact});
+        xml+=line;
         line = reader.readLine();
         }
-        return list.iterator();
+        XStream xStream = new XStream();
+        xStream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>)xStream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList())
+                .iterator();
 
     }
 
     @Test (dataProvider = "validContacts")
     public void testContactCreation(ContactData contact) {
-            app.goTo().groupPage();
-            GroupData group = new GroupData().withName("test")
-                    .withHeader("header").withFooter("footer");
-            app.group().create(group);
             app.goTo().homePage();
             Contacts before = app.contact().all();
             app.contact().create(contact);
